5438b0d23c24f535a23f56c8700284cabc9517ec
Lasse Claes Jacobsen <lacj@Lasses-MacBook-Pro.local>
2020-04-20 03:38:06 +0200

    a little cleanup, removed hr added border-bottom, changed the grid a bit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # bike_w_subpages
+
+Exercise: recreate this - 
+https://ui-patterns.com/patterns/cards/examples/17796
+
+Originally forked from https://github.com/annadagbjort/recreate-bike/
+
+I needed a fast starting point. I've done a little cleanup: removed hr-elements and added borders etc. but much of the code is Annas.
---
+++ js/script.js
@@ -0,0 +1,60 @@
+window.addEventListener('DOMContentLoaded', getData);
+
+const datalink = "https://annadagbjort.dk/cms-theme/bikes/wp-json/wp/v2/bike?_embed";
+
+const datalink2 = "https://lasseclaes.com/20f/2nd_sem_int/wp/wp-json/wp/v2/bike?_embed";
+
+function getData() {
+  //console.log('DOM fully loaded and parsed');
+  fetch(datalink)
+    .then(res => res.json())
+    .then(handleData)
+}
+
+function handleData(posts) {
+  console.log(posts);
+  posts.forEach(showBikes)
+}
+
+function showBikes(bikes) {
+  console.log(bikes);
+  const template = document.querySelector("template").content;
+
+  const copy = template.cloneNode(true);
+
+  console.log(bikes._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url)
+
+  copy.querySelector(".brand").textContent = bikes.brand;
+
+  copy.querySelector(".model").textContent = bikes.model;
+  copy.querySelector(".price").textContent = bikes.price;
+  copy.querySelector(".toPrice").textContent = bikes.price_to;
+
+  //    copy.querySelector(".colour").textContent = bikes.colours;
+
+  copy.querySelector(".inStock").textContent = bikes.in_stock;
+
+
+  copy.querySelector(".img-bike").src = bikes._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url
+  copy.querySelector(".img-bike").alt = bikes.brand;
+
+  if (bikes.price_to == false) {
+    copy.querySelector(".twoPrices").classList.add("hide");
+  }
+
+  const colorArray = bikes.colours.split(",");
+
+  colorArray.forEach(color => {
+    const col = document.createElement("div")
+    col.classList.add("colourDiv");
+    col.style.background = color;
+    copy.querySelector(".colour").appendChild(col)
+  })
+
+  if (bikes.colours == false) {
+    copy.querySelector(".colour").textContent = ("N/A");
+  }
+
+
+  document.querySelector("main").appendChild(copy);
+}
