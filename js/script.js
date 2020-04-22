bb92442dfe850f606b9bf3828d3c8baa10518e82
Lasse Claes Jacobsen <lacj@Lasses-MacBook-Pro.local>
2020-04-22 20:06:42 +0200

    a little more clean up - and messing around

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 window.addEventListener('DOMContentLoaded', getData);
 
-const datalink = "https://annadagbjort.dk/cms-theme/bikes/wp-json/wp/v2/bike?_embed";
-
+//const datalink = "https://annadagbjort.dk/cms-theme/bikes/wp-json/wp/v2/bike?_embed"; - thanks ;-)
 const datalink2 = "https://lasseclaes.com/20f/2nd_sem_int/wp/wp-json/wp/v2/bike?_embed";
 
 function getData() {
@@ -13,50 +12,48 @@ function getData() {
 
 function handleData(posts) {
   console.log(posts);
-  posts.forEach(showBikes)
+  posts.forEach(showBike);
 }
 
-function showBikes(bikes) {
-  console.log(bikes);
+function showBike(bike) {
+  console.log(bike);
   const template = document.querySelector("template").content;
-
   const copy = template.cloneNode(true);
-
-  console.log(bikes._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url)
+  console.log(bike._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url)
 
   //lacj: copy.querySelector(".brand").textContent = bikes.brand;
 
-  copy.querySelector(".brand").textContent = bikes._embedded["wp:term"][0][1].name;
+  copy.querySelector(".brand").textContent = bike._embedded["wp:term"][0][1].name;
   //  console.log('hey')
   //  console.log(bikes._embedded["wp:term"]);
 
   //lacj: copy.querySelector(".model").textContent = bikes.model;
-  copy.querySelector(".model").textContent = bikes.title.rendered;
-  copy.querySelector(".price").textContent = bikes.price;
-  copy.querySelector(".toPrice").textContent = bikes.price_to;
+  copy.querySelector(".model").textContent = bike.title.rendered;
+  copy.querySelector(".price").textContent = bike.price;
+  copy.querySelector(".toPrice").textContent = bike.price_to;
 
   //    copy.querySelector(".colour").textContent = bikes.colours;
 
-  copy.querySelector(".inStock").textContent = bikes.in_stock;
+  copy.querySelector(".inStock").textContent = bike.in_stock;
 
 
-  copy.querySelector(".img-bike").src = bikes._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url
-  copy.querySelector(".img-bike").alt = bikes.brand;
+  copy.querySelector(".img-bike").src = bike._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url
+  copy.querySelector(".img-bike").alt = bike.brand;
 
-  if (bikes.price_to == false) {
+  if (bike.price_to == false) {
     copy.querySelector(".twoPrices").classList.add("hide");
   }
 
-  const colorArray = bikes.colours.split(",");
+  const colorArray = bike.colours.split(",");
 
   colorArray.forEach(color => {
-    const col = document.createElement("div")
+    const col = document.createElement("div");
     col.classList.add("colourDiv");
     col.style.background = color;
     copy.querySelector(".colour").appendChild(col)
   })
 
-  if (bikes.colours == false) {
+  if (bike.colours == false) {
     copy.querySelector(".colour").textContent = ("N/A");
   }
 
